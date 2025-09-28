2ef25182349ac934128180aee3692cc10374a3f9
wiwams <wheniwasamedstud@gmail.com>
2025-09-28 13:41:29 +0700

    Add files via upload

---
+++ examCodes.js
@@ -0,0 +1,7 @@
+// kode ujian per kategori (boleh bebas, tidak harus 15 digit)
+const examCodes = {
+  "cbt": ["CBT1", "CBT2"],
+  "herdini": ["HER1", "HER2"],
+  "praktikum": ["PRAKT1", "PRAKT2"],
+  "ospe": ["OSPE1", "OSPE2"]
+};
---
+++ loginCodes.js
@@ -0,0 +1,9 @@
+// daftar kode login 124 anak (contoh saya isi 5 dulu)
+const validLogins = [
+  "123456789012345",
+  "123456789012346",
+  "123456789012347",
+  "123456789012348",
+  "123456789012349"
+  // tambahkan sampai 124
+];
